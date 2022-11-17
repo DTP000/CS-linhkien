94f1815033432d6db767ab7c5c273156360a7ec2
Phan Đình Trung <khoicantk@gmail.com>
2022-11-18 01:37:01 +0700

    add md5 login and login as username, phone and email

---
+++ Wpf.Ui.SimpleDemo/Data/Dao/UserDao.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Wpf.Ui.SimpleDemo.Data.Dao
+{
+    public interface UserDao
+    {
+        void insert(User user);
+        void update(User user);
+        List<User> findAll();
+        int count();
+        User findById(int id);
+        void deleteById(int id);
+        User find(string phone, string password);
+
+        User login(string username, string password);
+    }
+}
